09e28877832e0f2389729c8793ccfb6872323977
oto-webdev <r.otowebdev@gmail.com>
2024-11-10 23:37:02 +0400

    contact page

---
+++ pages/contact.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html lang="ka">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    <meta name="keywords" content="IT აუთსორსინგი, პროფესიონალური ვებდეველოპმენტი, SEO სერვისები ბიზნესებისთვის, ხელმისაწვდომი IT გადაწყვეტილებები, ქართული IT სერვისები, ტექნოლოგიური გადაწყვეტილებები, აიტი აუთსორსინგი">
+
+    <link rel="icon" href="./assets/logo.png" type="image/png">
+    <link rel="apple-touch-icon" href="./assets/logo.png">
+
+    <title>Clicks.ge - IT აუთსორსინგი</title>
+    <meta name="description" content="Clicks.ge გთავაზობთ პროფესიონალურ IT აუთსორსინგს, ვებგვერდების დამზადებას, SEO ოპტიმიზაციას და სხვა. გააუმჯობესეთ თქვენი ბიზნესი ჩვენი მაღალი ხარისხის ტექნოლოგიური გადაწყვეტილებებით.">
+    <meta name="author" content="Clicks.ge">
+    <meta name="robots" content="index, follow">
+
+    <meta property="og:title" content="Clicks.ge - IT სერვისები და გადაწყვეტილებები">
+    <meta property="og:description" content="აღმოაჩინეთ ჩვენი IT სერვისების რანგი, მათ შორის აუთსორსინგი, ვებგვერდების დამზადება და SEO ოპტიმიზაცია. დაეხმარეთ თქვენს ბიზნესს ჩვენი მაღალი ხარისხის გადაწყვეტილებებით.">
+    <meta property="og:image" content="https://clicks.ge/assets/og-image.png">
+    <meta property="og:url" content="https://clicks.ge">
+    <meta property="og:type" content="website">
+
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:title" content="Clicks.ge - IT სერვისები და გადაწყვეტილებები">
+    <meta name="twitter:description" content="IT სერვისები, რომლებიც აიყვანენ თქვენს ბიზნესს მომდევნო დონეზე. შეისწავლეთ ჩვენი IT აუთსორსინგი, ვებგვერდების დამზადება და SEO ოპტიმიზაცია.">
+    <meta name="twitter:image" content="https://clicks.ge/assets/twitter-image.png">
+    <meta name="twitter:site" content="@clicksge">
+    <meta name="twitter:creator" content="@clicksge">
+
+    <link rel="canonical" href="https://clicks.ge">
+
+    <link rel="stylesheet" href="../css/styles.css">
+    <link rel="stylesheet" href="../css/base.css">
+    <link rel="stylesheet" href="./styles.css">
+
+    <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer">
+
+    <meta name="theme-color" content="#000000">
+
+    <script src="https://static.elfsight.com/platform/platform.js" async></script>
+
+</head>
+
+<body class="">
+
+    <!-- live chat -->
+    <div class="elfsight-app-9938ab94-1306-4e69-895b-2d3392c29c39" data-elfsight-app-lazy></div>
+
+    <!--fixed icons -->
+    <div class="controls">
+        <?php include "../components/control.html" ?>
+    </div>
+
+    <!-- scroll to top btn -->
+    <button id="scrollToTopBtn">↑</button>
+
+    <header class="header">
+        <?php include '../components/navbar.html' ?>
+    </header>
+
+    <main>
+
+        <section class="contact">
+            <h1>დაგვეკონტაქტე</h1>
+            <div class="contacts">
+                <div class="left">
+                    <form action="#" method="POST">
+                        <input type="text" name="firstname" placeholder="სახელი">
+                        <input type="text" name="lastname" placeholder="გვარი">
+                        <input type="email" name="email" placeholder="მეილი">
+                        <textarea name="text" id="text" placeholder="მესიჯი"></textarea>
+                        <button type="submit">გაგზავნა</button>
+                    </form>
+                </div>
+
+                <div class="right">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3107.219238911673!2d44.726198961486304!3d41.72451038460709!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4044730c1f047a25%3A0x68e81556c46907c9!2s71%20Vazha-Pshavela%20Ave%2C%20T&#39;bilisi!5e1!3m2!1sen!2sge!4v1731265576275!5m2!1sen!2sge" width="600" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    <div class="info">
+                        <h2>ინფორმაცია</h2>
+                        <hr><br>
+                        <div>
+                            <h2>მისამართი</h2>
+                            <p>ვაჟა-ფშაველას N71</p>
+                            <p>ორშ-პარ: 10:00-19:00</p>
+                        </div><br>
+                        <div>
+                            <h2>მეილი</h2>
+                            <p>info@click.ge</p>
+                            <p>zura_ramishvili@click.ge</p>
+                            <p>misha_mironov@click.ge</p>
+                        </div><br>
+                        <div>
+                            <h2>ტელეფონი</h2>
+                            <p>+(995) 577 99 42 99</p>
+                            <p>+(995) 599 99 93 09</p>
+                            <p>+(995) 577 02 24 14</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+    </main>
+
+    <footer class="footer">
+        <?php include "../components/footer.html" ?>
+    </footer>
+
+    <script src="../javascript/main.js"></script>
+</body>
+
+</html>
