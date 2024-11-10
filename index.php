8fe6a464d027e4def50ce8a3e18fd8ce090d6413
oto-webdev <r.otowebdev@gmail.com>
2024-11-10 12:45:31 +0400

    add components

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ka">
 
 <head>
     <meta charset="UTF-8">
@@ -48,15 +48,7 @@
 
     <!--fixed icons -->
     <div class="controls">
-        <div class="control" data-id="about">
-            <i class="fa-brands fa-facebook"></i>
-        </div>
-        <div class="control" data-id="portfolio">
-            <i class="fa-brands fa-instagram"></i>
-        </div>
-        <div class="control" data-id="blogs">
-            <i class="fa-brands fa-tiktok"></i>
-        </div>
+        <?php include "./components/control.html" ?>
     </div>
 
     <!-- scroll to top btn -->
@@ -69,257 +61,33 @@
     <main>
 
         <section class="home">
-            <div class="swiper-container">
-                <div class="swiper-wrapper">
-                    <div class="swiper-slide">
-                        <img src="./assets/1.jpg" alt="Slide 1">
-                        <div class="slide-content">
-                            <h1>ჩვენი გუნდი, დაგიმზადებთ მარტივ საიტებს ხელმისაწვდომ ფასებში</h1>
-                        </div>
-                    </div>
-                    <div class="swiper-slide">
-                        <img src="./assets/2.jpg" alt="Slide 2">
-                        <div class="slide-content">
-                            <h1>მზად ხართ ვებსაიტის შესაქმნელად? დაგვიკავშირდით უფასო კონსულტაციისთვის</h1>
-                        </div>
-                    </div>
-                    <div class="swiper-slide">
-                        <img src="./assets/3.jpg" alt="Slide 3">
-                        <div class="slide-content">
-                            <h1>ჩვენ ვაქცევთ თქვენს ხედვას რეალობად.</h1>
-                        </div>
-                    </div>
-                </div>
-                <div class="swiper-button-next"></div>
-                <div class="swiper-button-prev"></div>
-            </div>
+            <?php include "./components/home.html" ?>
         </section>
 
         <section class="service">
-            <div class="service_list">
-                <div>
-                    <i class="fa-solid fa-desktop"></i>
-                    <p>ტექნიკური სერვისი</p>
-                </div>
-                <div>
-                    <i class="fa-solid fa-wifi"></i>
-                    <p>ქსელები</p>
-                </div>
-                <div>
-                    <i class="fa-solid fa-list"></i>
-                    <p>სერვერები</p>
-                </div>
-                <div>
-                    <i class="fa-solid fa-phone"></i>
-                    <p>IP ტელეფონი</p>
-                </div>
-                <div>
-                    <i class="fa-solid fa-barcode"></i>
-                    <p>პროგრამული სერვისი</p>
-                </div>
-                <div>
-                    <i class="fa-solid fa-mobile-screen"></i>
-                    <p>სმარტფონების სერვისი</p>
-                </div>
-                <div>
-                    <i class="fa-solid fa-video"></i>
-                    <p>კამერები სისტემები</p>
-                </div>
-                <div>
-                    <i class="fa-solid fa-cloud"></i>
-                    <p>ვირტუალური სერვერები</p>
-                </div>
-            </div>
+            <?php include "./components/service.html" ?>
         </section>
 
         <section class="completed-projects">
-            <div class="completed-project">
-                <div>
-                    <p>2</p>
-                    <h1>წარმატებული წელი ბაზარზე</h1>
-                </div>
-                <div>
-                    <p>37</p>
-                    <h1>შესრულებული პროექტები</h1>
-                </div>
-                <div>
-                    <p>1</p>
-                    <h1>მიმდინარე პროექტები</h1>
-                </div>
-            </div>
-        </section>
-        
-        <section class="options">
-            <div class="option-list">
-                <div class="card">
-                    <h1>500 + ლარი</h1>
-                    <h2>მინი ვებ გვერდი</h2>
-
-                    <div class="card_list">
-                        <ul>
-                            <li><i class="fa-solid fa-check"></i>
-                                <p>1-7 გვერდი</p>
-                            </li>
-                            <li><i class="fa-solid fa-check"></i>
-                                <p>ორი ენა</p>
-                            </li>
-                            <li><i class="fa-solid fa-check"></i>
-                                <p>საკონტაქტო ფორმა</p>
-                            </li>
-                            <li><i class="fa-solid fa-check"></i>
-                                <p>მობილური ვერსია</p>
-                            </li>
-                            <li><i class="fa-solid fa-check"></i>
-                                <p>google maps</p>
-                            </li>
-                        </ul>
-                    </div>
-                    <button>შეუკვეთე</button>
-                </div>
-
-                <div class="card">
-                    <h1>995 + ლარი</h1>
-                    <h2>საშუალო ვებ გვერდი</h2>
-
-                    <div class="card_list">
-                        <ul>
-                            <li><i class="fa-solid fa-check"></i>
-                                <p>7-10 გვერდი</p>
-                            </li>
-                            <li><i class="fa-solid fa-check"></i>
-                                <p>სამი ენა</p>
-                            </li>
-                            <li><i class="fa-solid fa-check"></i>
-                                <p>messenger ჩატი</p>
-                            </li>
-                            <li><i class="fa-solid fa-check"></i>
-                                <p>ყველა ვერსია</p>
-                            </li>
-                            <li><i class="fa-solid fa-check"></i>
-                                <p>google maps</p>
-                            </li>
-                        </ul>
-                    </div>
-                    <button>შეუკვეთე</button>
-                </div>
-
-                <div class="card">
-                    <h1>1500 + ლარი</h1>
-                    <h2>ბიზნეს ვებ გვერდი</h2>
-
-                    <div class="card_list">
-                        <ul>
-                            <li><i class="fa-solid fa-check"></i>
-                                <p>10+ გვერდი</p>
-                            </li>
-                            <li><i class="fa-solid fa-check"></i>
-                                <p>სამი ენა</p>
-                            </li>
-                            <li><i class="fa-solid fa-check"></i>
-                                <p>Single Page Application</p>
-                            </li>
-                            <li><i class="fa-solid fa-check"></i>
-                                <p>SEO ოპტიმიზაცია</p>
-                            </li>
-                            <li><i class="fa-solid fa-check"></i>
-                                <p>პინის დასმა</p>
-                            </li>
-                        </ul>
-                    </div>
-                    <button>შეუკვეთე</button>
-                </div>
-            </div>
+            <?php include "./components/complete-project.html" ?>
         </section>
 
         <section class="my-service">
-            <div class="service-cards">
-                <div class="service-card">
-                    <h2>01.</h2>
-                    <hr>
-                    <div>
-                        <i class="fa-solid fa-list"></i>
-                        <h1>IT აუთსორსინგი</h1>
-                    </div>
-                    <p>IT აუთსორსინგი არის ტექნოლოგიური რესურსებისა და სერვისების გარე პარტნიორებისგან მიღების პროცესი, რაც კომპანიას საშუალებას აძლევს, ფოკუსირდეს ძირითად საქმიანობაზე, შეამციროს ხარჯები და გაზარდოს მოქნილობა.</p>
-                </div>
-                <div class="service-card">
-                    <h2>02.</h2>
-                    <hr>
-                    <div>
-                        <i class="fa-solid fa-desktop"></i>
-                        <h1>საიტების დამზადება</h1>
-                    </div>
-                    <p>
-                    საიტების დამზადება გულისხმობს კომპანიის ონლაინ იდენტობის შექმნას და განვითარებას, რაც ხელს უწყობს ბრენდის ცნობადობის გაზრდას, მომხმარებელთა მოზიდვას და კონკურენტულობის გაძლიერებას.</p>
-                </div>
-                <div class="service-card">
-                    <h2>03.</h2>
-                    <hr>
-                    <div>
-                        <i class="fa-solid fa-chart-bar"></i>
-                        <h1>SEO ოპტიმიზაცია</h1>
-                    </div>
-                    <p>SEO ოპტიმიზაცია გულისხმობს ვებგვერდის გაუმჯობესებას საძიებო სისტემებში უკეთესი პოზიციების მოსაპოვებლად, რაც ზრდის მისი ნახვების რაოდენობას და მომხმარებელთა ჩართულობას.</p>
-                </div>
-            </div>
+            <?php include "./components/my-service.html" ?>
         </section>
 
         <section class="calculator">
-            <div>
-                <h1>სერვისების კალკულატორი</h1>
-            </div>
-            <div class="display">
-                <div>
-                    <h2>კომპიუტერები</h2>
-                    <input type="number" class="inp-num" id="computers" placeholder="რაოდენობა">
-                    <p id="comp-price"></p>
-                </div>
-                <div>
-                    <h2>პრინტერები</h2>
-                    <input type="number" class="inp-num" id="printers" placeholder="რაოდენობა">
-                    <p id="print-price"></p>
-                </div>
-                <div>
-                    <h2>სერვერები</h2>
-                    <input type="number" class="inp-num" id="servers" placeholder="რაოდენობა">
-                    <p id="server-price"></p>
-                </div>
-            </div>
-            <div class="calculate">
-                <button type="button" onclick="calculatePrice()">გამოთვალე</button>
-                <div class="result">
-                    <h3 id="total-price">0 დოლარი</h3>
-                </div>
-            </div>
+            <?php include "./components/calculator.html" ?>
         </section>
 
         <section class="project">
-            <div class="projects">
-                <div class="project-card">
-                    <img src="./assets/card1.jpg" alt="ვიდეო კამერები">
-                    <p>ვიდეო კამერები გამოიყენება უსაფრთხოების, მონიტორინგისა და ვიზუალური ჩანაწერების შესაქმნელად. თანამედროვე კამერების საშუალებით შესაძლებელია მაღალი ხარისხის გამოსახულების მიღება, რეალურ დროში მონიტორინგი და ჩანაწერების შენახვა.</p>
-                </div>
-                <div class="project-card">
-                    <img src="./assets/card2.jpg" alt="ანტივირუსი">
-                    <p>ანტივირუსი იცავს კომპიუტერულ სისტემებს ვირუსების, მავნე პროგრამების და კიბერშეტევებისგან. მისი საშუალებით ხდება სისტემის სკანირება, მავნე კოდის ამოცნობა და მათგან დაცვა, რაც უზრუნველყოფს მონაცემების უსაფრთხოებას და მოწყობილობის გამართულ მუშაობას.</p>
-                </div>
-                <div class="project-card">
-                    <img src="./assets/card3.jpg" alt="IT აუთსორსინგის უპირატესობები">
-                    <p>IT აუთსორსინგი საშუალებას იძლევა კომპანიებს თავიანთი რესურსები უფრო ეფექტურად მართონ. გარე პარტნიორებთან თანამშრომლობა უზრუნველყოფს მაღალი ხარისხის ტექნოლოგიურ მომსახურებას, აჩქარებს პროცესებს და ხარჯებს ამცირებს.</p>
-                </div>
-                <div class="project-card">
-                    <img src="./assets/card4.jpg" alt="Office 365">
-                    <p>Office 365 არის Microsoft-ის გამოწერის მომსახურება, რომელიც აერთიანებს სხვადასხვა ოფის აპლიკაციებს, როგორიცაა Word, Excel, PowerPoint, Outlook და OneDrive. ის უზრუნველყოფს ციფრულ პროდუქტებს და სერვისებს, რაც საშუალებას აძლევს მომხმარებლებს კოლაბორაციისა და სამუშაოების უფრო მოქნილად შესრულებას.</p>
-                </div>
-            </div>
+            <?php include "./components/project.html" ?>
         </section>
 
     </main>
 
     <footer class="footer">
-        <div>
-            <p>© 2024 Clicks.ge | ყველა უფლება დაცულია.</p>
-        </div>
+        <?php include "./components/footer.html" ?>
     </footer>
 
     <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
