1905c18d73127c4ed2418c3ea20391c5b747860c
oto-webdev <r.otowebdev@gmail.com>
2024-11-10 15:09:18 +0400

    about page

---
+++ pages/about.php
@@ -0,0 +1,143 @@
+<!DOCTYPE html>
+<html lang="ka">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    <link rel="icon" href="../assets/logo.png" type="image/png">
+    <link rel="apple-touch-icon" href="../assets/logo.png">
+
+    <title>Clicks.ge - ჩვენს შესახებ</title>
+    <meta name="description" content="გაიცანით Clicks.ge - კომპანია, რომელიც გთავაზობთ IT აუთსორსინგის, ვებგვერდების დამზადების და SEO ოპტიმიზაციის სერვისებს. ჩვენი გუნდი მიზნად ისახავს მაღალი ხარისხის IT გადაწყვეტილებების მიწოდებას.">
+    <meta name="keywords" content="Clicks.ge, ჩვენს შესახებ, IT კომპანია, ქართული IT სერვისები, IT აუთსორსინგი">
+    <meta name="author" content="Clicks.ge">
+    <meta name="robots" content="index, follow">
+
+    <meta property="og:title" content="Clicks.ge - ჩვენს შესახებ">
+    <meta property="og:description" content="Clicks.ge არის პროფესიონალური IT აუთსორსინგის და ვებ-დამზადების კომპანია, რომელიც ეხმარება ბიზნესებს განვითარებაში და ზრდაში.">
+    <meta property="og:image" content="https://clicks.ge/assets/og-image.png">
+    <meta property="og:url" content="https://clicks.ge/about">
+    <meta property="og:type" content="website">
+
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:title" content="Clicks.ge - ჩვენს შესახებ">
+    <meta name="twitter:description" content="გაიგეთ მეტი Clicks.ge-ის შესახებ და აღმოაჩინეთ ჩვენი გუნდის მიზნები და ღირებულებები.">
+    <meta name="twitter:image" content="https://clicks.ge/assets/twitter-image.png">
+    <meta name="twitter:site" content="@clicksge">
+    <meta name="twitter:creator" content="@clicksge">
+
+    <link rel="canonical" href="https://clicks.ge/about">
+
+    <link rel="stylesheet" href="../css/styles.css">
+    <link rel="stylesheet" href="../css/base.css">
+    <link rel="stylesheet" href="./styles.css">
+    <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer">
+
+    <meta name="theme-color" content="#000000">
+
+    <script src="https://static.elfsight.com/platform/platform.js" async></script>
+
+</head>
+
+<body>
+
+    <!-- live chat -->
+    <div class="elfsight-app-9938ab94-1306-4e69-895b-2d3392c29c39" data-elfsight-app-lazy></div>
+
+    <!-- fixed icons -->
+    <div class="controls">
+        <?php include "../components/control.html" ?>
+    </div>
+
+    <!-- scroll to top button -->
+    <button id="scrollToTopBtn">↑</button>
+
+    <header class="header">
+        <?php include '../components/navbar.html' ?>
+    </header>
+
+    <main>
+
+        <section class="about">
+            <div class="container">
+                <h1>ჩვენს შესახებ</h1>
+                <p>Clicks.ge არის IT კომპანია, რომელიც მიზნად ისახავს ბიზნესების განვითარების და ზრდის მხარდაჭერას მაღალი ხარისხის ტექნოლოგიური გადაწყვეტილებებით. ჩვენი გუნდი გთავაზობთ პროფესიონალურ IT აუთსორსინგის სერვისებს, ვებგვერდების დამზადებას და SEO ოპტიმიზაციას.</p>
+
+                <div class="about-card">
+                    <div class="about-cards">
+                        <div>
+                            <h2>საიტების დამზადება</h2>
+                            <p>ჩვენ გთავაზობთ პროფესიონალური და მომხმარებლის საჭიროებებზე მორგებულ ვებგვერდების დამზადებას. ჩვენი გუნდი იყენებს უახლეს ტექნოლოგიებს, რათა შევქმნათ ინოვაციური და ფუნქციური საიტები, რომლებიც ოპტიმიზებულია როგორც ვიზუალურად, ასევე ტექნიკურად.</p>
+                        </div>
+                        <div>
+                            <h2>IT აუთსორსინგი</h2>
+                            <p>ჩვენი IT აუთსორსინგის სერვისი გეხმარებათ ოპტიმიზაციაში და ხარჯების შემცირებაში. მიგვმართეთ ტექნიკური მხარდაჭერის, სისტემების მართვისა და სხვა IT მოთხოვნებისათვის. ჩვენ გთავაზობთ პროფესიონალ გუნდს და შესაბამის გამოცდილებას, რათა უზრუნველვყოთ თქვენი IT პროცესების სტაბილური და უსაფრთხო მუშაობა.</p>
+                        </div>
+                        <div>
+                            <h2>SEO ოპტიმიზაცია</h2>
+                            <p>SEO ოპტიმიზაცია უზრუნველყოფს თქვენი ვებგვერდის ხილვადობის ზრდას საძიებო სისტემებში. ჩვენი SEO ექსპერტები შეიმუშავებენ სტრატეგიას, რომელიც მოიცავს საკვანძო სიტყვების კვლევას, კონტენტის ოპტიმიზაციას და ბმულების შექმნას, რაც ხელს უწყობს თქვენი საიტის აუდიტორიის გაზრდას.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <div class="skills">
+            <h1>ჩვენი უნარები</h1>
+
+            <div class="skill">
+                <div class="skills-category">
+                    <h2>frontend</h2>
+                    <div class="skills-grid">
+                        <div class="skill-item">
+                            <h3>Html</h3>
+                            <h3>CSS</h3>
+                            <h3>JavaScript</h3>
+                            <h3>React</h3>
+                            <h3>WordPress</h3>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="skills-category">
+                    <h2>backend</h2>
+                    <div class="skills-grid">
+                        <div class="skill-item">
+                            <h3>PHP</h3>
+                            <h3>Django</h3>
+                            <h3>Node.js</h3>
+                            <h3>MySQL</h3>
+                            <h3>MongoDB</h3>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="skills-category">
+                    <h2>სხვა უნარები</h2>
+                    <div class="skills-grid">
+                        <div class="skill-item">
+                            <h3>SEO ოპტიმიზაცია</h3>
+                            <h3>IT აუთსორსინგი</h3>
+                            <h3>Git & GitHub</h3>
+                            <h3>Single Page Application</h3>
+                            <h3>ვებსაიტები</h3>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </main>
+
+    <footer class="footer">
+        <?php include "../components/footer.html" ?>
+    </footer>
+
+    <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
+    <script src="../javascript/main.js"></script>
+</body>
+
+</html>
